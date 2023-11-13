b27600c01a0c8c1b6ea99781040e2211aec46394
Rodion Mern <114694164+rodionmern@users.noreply.github.com>
2023-11-13 20:58:38 +0300

    Alpha 0.0.1.#small_update
    fix end alert
    and small correcting

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+const FirstNum = document.getElementById("first_number");
+const SecondNum = document.getElementById("second_number");
+const Solution = document.getElementById("solution");
+
 const first = () => {
 
     first_number = +prompt("Введите число");
 
-    localStorage.setItem('first__num', first_number);
+    localStorage.setItem('first__num', first_number);   
 
 }
 
@@ -20,22 +24,29 @@ const solve = () => {
 
     if (symbol__num == "+") {
         let solution = localStorage.setItem('solve__numbers', first_number + second_number);
-    } else if (symbol__num == "-") {
+    } 
+    else if (symbol__num == "-") {
         let solution = localStorage.setItem('solve__numbers', first_number - second_number);
-    } else if (symbol__num == "*") {
+    } 
+    else if (symbol__num == "*") {
         let solution = localStorage.setItem('solve__numbers', first_number * second_number);
-    } else if (symbol__num == "**") {
+    } 
+    else if (symbol__num == "**") {
         let solution = localStorage.setItem('solve__numbers', first_number ** second_number);
-    } else if (symbol__num == "/") {
+    } 
+    else if (symbol__num == "/") {
         let solution = localStorage.setItem('solve__numbers', first_number / second_number);
-    } else if (symbol__num == "%") {
+    } 
+    else if (symbol__num == "%") {
         let solution = localStorage.setItem('solve__numbers', first_number ** second_number);
-    } else {
-        alert("Вводите верное значение")
+    } 
+    else {
+        alert("Вы ввели неверное значения!")
     }
 
     // console.log("Сложение - ", first_number, " и ", second_number, " равно ", localStorage.getItem('solve__numbers'));
 
     alert(localStorage.getItem('solve__numbers'));
 
+    console.log("Alert успешно выведен.")
 }
